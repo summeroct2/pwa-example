b1a15a83600b5968765b33ec0b739e1686891fb0
夏燕飞 <xiayanfei@meizu.com>
2018-09-07 18:15:49 +0800

    注册service worker

--- public/sw-register.js
+++ public/sw-register.js
@@ -1,19 +1,6 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js?v=20180904153513').then(reg => {
-    reg.onupdatefound = function() {
-      var installingWorker = reg.installing;
-      installingWorker.onstatechange = function() {
-        switch (installingWorker.state) {
-          case 'installed':
-            if (navigator.serviceWorker.controller) {
-              var event = document.createEvent('Event');
-              event.initEvent('sw.update', true, true);
-              window.dispatchEvent(event)
-            }
-            break;
-        }
-      }
-    }
+  navigator.serviceWorker.register('/service-worker.js').then(reg => {
+    console.log('service worker register success')
   }).catch(e => {
     console.error('Error during service worker registration:', e)
   })
