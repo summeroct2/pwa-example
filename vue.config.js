a6d7db9a34d8205f27d5444bc032b1dc1379c63e
summeroct2 <hixiayanfei@gmail.com>
2018-09-07 16:31:01 +0800

    基础功能配置

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  devServer: {
+    // host: 'test.api.douban.com',
+    proxy: {
+      '/api': {
+        target: 'https://api.douban.com/v2',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/'
+        }
+      }
+    }
+  },
+  // pwa: {
+  //   name: '正在热映x',
+  // }
+}
