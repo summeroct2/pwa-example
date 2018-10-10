172e5f9c4c22865b08f09e3cea64cc8e1e2f49bc
summeroct2 <hixiayanfei@gmail.com>
2018-10-10 14:08:32 +0800

    取消https，可用localhost本地调试

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,13 @@ const fs = require('fs')
 module.exports = {
   devServer: {
     // https: true,
-    https: {
-      key: fs.readFileSync("./server.key"),
-      cert: fs.readFileSync("./server.crt"),
-    },
+    // https: {
+    //   key: fs.readFileSync("./server.key"),
+    //   cert: fs.readFileSync("./server.crt"),
+    // },
     // host: 'test.api.douban.com',
     // http://news-at.zhihu.com/api/4/news/latest
+
     proxy: {
       '/api': {
         // target: 'https://api.douban.com/v2',
